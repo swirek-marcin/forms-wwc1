09a11f1b50b35c445f24cb07c8d1323645ce5137
Marcin Świrek <swirek.marcin@gmail.com>
2018-12-29 23:27:21 +0100

    Add basic API validation and sample of validation using JS as a comment in main.js file

---
+++ js/main.js
@@ -0,0 +1,32 @@
+//A sample of validation using JS
+
+// const formOne = document.querySelector('.register-form');
+// const error = document.querySelector('.error');
+// const registerMail = document.querySelector('.email-input-one');
+
+//Form one email input validation===============================
+
+// registerMail.addEventListener(
+//   'input',
+//   e => {
+//     if (registerMail.validity.valid) {
+//       error.innerHTML = '';
+//       error.className = 'error';
+//     }
+//   },
+//   false
+// );
+
+// formOne.addEventListener(
+//   'submit',
+//   e => {
+//     if (!registerMail.validity.valid || registerMail.value == '') {
+//       error.innerHTML = 'Please, enter a proper email address...';
+//       error.className = 'error active';
+//       e.preventDefault();
+//     }
+//   },
+//   false
+// );
+
+
